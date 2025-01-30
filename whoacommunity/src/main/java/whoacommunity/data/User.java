1a70f657cdd7eca8f066bd90c4b448d6778dfd9b
Hugi Thordarson <hugi@karlmenn.is>
2025-01-30 11:45:32 +0000

    Adding cayenne

---
+++ whoacommunity/src/main/java/whoacommunity/data/User.java
@@ -0,0 +1,7 @@
+package whoacommunity.data;
+
+import jambalaya.interfaces.DateTimeStamped;
+import jambalaya.interfaces.UUIDStamped;
+import whoacommunity.data.auto._User;
+
+public class User extends _User implements DateTimeStamped, UUIDStamped {}
