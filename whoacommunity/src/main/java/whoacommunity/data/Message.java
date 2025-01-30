ffd1dfa39508b52ca33958067d4d807dc6c93456
Hugi Thordarson <hugi@karlmenn.is>
2025-01-30 17:29:22 +0000

    Slight cleanup in message HTML rendering

--- whoacommunity/src/main/java/whoacommunity/data/Message.java
+++ whoacommunity/src/main/java/whoacommunity/data/Message.java
@@ -2,11 +2,22 @@ package whoacommunity.data;
 
 import jambalaya.interfaces.DateTimeStamped;
 import jambalaya.interfaces.UUIDStamped;
+import ng.appserver.privates.NGHTMLUtilities;
 import whoacommunity.data.auto._Message;
 
 public class Message extends _Message implements DateTimeStamped, UUIDStamped {
 
+	/**
+	 * @return The text for HTML display
+	 *
+	 * FIXME: Somewhat temporary until we've parsed the actual rich message content
+	 */
 	public String textRendered() {
-		return text().replace( "\n", "<br>\n" );
+		String text = text();
+
+		text = NGHTMLUtilities.escapeHTML( text );
+		text = text.replace( "\n", "<br>\n" );
+
+		return text;
 	}
 }
