4db88826881a50910572866dfa672ff6e30f455b
Hugi Thordarson <hugi@karlmenn.is>
2025-01-30 13:25:20 +0000

    Added the search page

---
+++ whoacommunity/src/main/java/whoacommunity/components/WCSearchPage.java
@@ -0,0 +1,13 @@
+package whoacommunity.components;
+
+import ng.appserver.NGComponent;
+import ng.appserver.NGContext;
+
+public class WCSearchPage extends NGComponent {
+
+	public String searchString;
+
+	public WCSearchPage( NGContext context ) {
+		super( context );
+	}
+}
