0e3724441ef6707fbfd29f7c2f5934f21ae64d2a
Hugi Thordarson <hugi@karlmenn.is>
2025-01-30 16:41:21 +0000

    Render at least line breaks in the message text

--- whoacommunity/src/main/java/whoacommunity/data/Message.java
+++ whoacommunity/src/main/java/whoacommunity/data/Message.java
@@ -4,4 +4,9 @@ import jambalaya.interfaces.DateTimeStamped;
 import jambalaya.interfaces.UUIDStamped;
 import whoacommunity.data.auto._Message;
 
-public class Message extends _Message implements DateTimeStamped, UUIDStamped {}
+public class Message extends _Message implements DateTimeStamped, UUIDStamped {
+
+	public String textRendered() {
+		return text().replace( "\n", "<br>\n" );
+	}
+}
