7c94ee28d43912ad7da47c7e544fe66f57e358a5
Hugi Thordarson <hugi@karlmenn.is>
2025-01-30 13:23:58 +0000

    Temporary "Login" functionality (quotes being very intentional)

--- whoacommunity/src/main/java/whoacommunity/components/WCMain.java
+++ whoacommunity/src/main/java/whoacommunity/components/WCMain.java
@@ -1,20 +1,37 @@
 package whoacommunity.components;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.cayenne.query.ObjectSelect;
 
+import ng.appserver.NGActionResults;
 import ng.appserver.NGComponent;
 import ng.appserver.NGContext;
 import whoacommunity.app.WCCore;
 import whoacommunity.data.Channel;
+import whoacommunity.data.Message;
 
 public class WCMain extends NGComponent {
 
+	private boolean _isLoggedIn;
+
+	public String password;
+
 	public Channel currentChannel;
+	public Channel selectedChannel;
+	public Message currentMessage;
 
 	public WCMain( NGContext context ) {
 		super( context );
+	}
+
+	public NGActionResults login() {
+		if( "wocomponent".equals( password ) ) {
+			_isLoggedIn = true;
+		}
+
+		return null;
 	}
 
 	public List<Channel> channels() {
@@ -24,7 +41,38 @@ public class WCMain extends NGComponent {
 				.select( WCCore.newContext() );
 	}
 
-	public String currentChannelURL() {
-		return "/channel/" + currentChannel.name();
+	public NGActionResults selectChannel() {
+		selectedChannel = currentChannel;
+		return null;
+	}
+
+	/**
+	 * FIXME: Still considering if we actually want formatting
+	 */
+	public String currentMessageDate() {
+		return currentMessage.dateTime().toString();
+	}
+
+	public List<Message> messages() {
+
+		if( selectedChannel == null ) {
+			return Collections.emptyList();
+		}
+
+		final List<Message> messages = ObjectSelect
+				.query( Message.class )
+				.where( Message.CHANNEL.eq( selectedChannel ) )
+				.orderBy( Message.DATE_TIME.asc() )
+				.prefetch( Message.USER.joint() )
+				.select( WCCore.newContext() );
+
+		return messages
+				.stream()
+				.filter( f -> !"channel_join".equals( f.slackSubtype() ) )
+				.toList();
+	}
+
+	public boolean isLoggedIn() {
+		return _isLoggedIn;
 	}
 }
