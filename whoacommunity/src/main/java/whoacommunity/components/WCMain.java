65604088a81cf803b6bd3ff1dc573f3bac428359
Hugi Thordarson <hugi@karlmenn.is>
2025-01-30 13:06:00 +0000

    List channels in main page

--- whoacommunity/src/main/java/whoacommunity/components/WCMain.java
+++ whoacommunity/src/main/java/whoacommunity/components/WCMain.java
@@ -1,11 +1,30 @@
 package whoacommunity.components;
 
+import java.util.List;
+
+import org.apache.cayenne.query.ObjectSelect;
+
 import ng.appserver.NGComponent;
 import ng.appserver.NGContext;
+import whoacommunity.app.WCCore;
+import whoacommunity.data.Channel;
 
 public class WCMain extends NGComponent {
 
+	public Channel currentChannel;
+
 	public WCMain( NGContext context ) {
 		super( context );
+	}
+
+	public List<Channel> channels() {
+		return ObjectSelect
+				.query( Channel.class )
+				.orderBy( Channel.NAME.ascInsensitive() )
+				.select( WCCore.newContext() );
+	}
+
+	public String currentChannelURL() {
+		return "/channel/" + currentChannel.name();
 	}
 }
