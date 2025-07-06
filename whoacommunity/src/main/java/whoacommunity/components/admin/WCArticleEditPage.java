01e704ada05ed3bf0837b243fad1c6c002e2a122
Hugi Thordarson <hugi@karlmenn.is>
2025-07-06 21:49:10 +0000

    Add support for article deletion

--- whoacommunity/src/main/java/whoacommunity/components/admin/WCArticleEditPage.java
+++ whoacommunity/src/main/java/whoacommunity/components/admin/WCArticleEditPage.java
@@ -1,7 +1,10 @@
 package whoacommunity.components.admin;
 
+import org.apache.cayenne.ObjectContext;
+
 import ng.appserver.NGActionResults;
 import ng.appserver.NGContext;
+import ng.appserver.NGResponse;
 import whoacommunity.app.WCComponent;
 import whoacommunity.data.Article;
 import whoacommunity.data.Article.ArticleFormat;
@@ -19,7 +22,27 @@ public class WCArticleEditPage extends WCComponent {
 		return null;
 	}
 
+	public NGActionResults delete() {
+		final ObjectContext oc = selectedObject.getObjectContext();
+		oc.deleteObject( selectedObject );
+		oc.commitChanges();
+		return redirectTemporary( "/" );
+	}
+
 	public ArticleFormat[] articleFormats() {
 		return ArticleFormat.values();
+	}
+
+	/**
+	 * Creates a response with a temporary (302) redirect to the specified URL
+	 *
+	 * @param url The URL to redirect to
+	 */
+	private static NGResponse redirectTemporary( final String url ) {
+		final NGResponse response = new NGResponse();
+		response.setStatus( 302 );
+		response.setHeader( "location", url );
+		response.setHeader( "content-type", "text/html" );
+		return response;
 	}
 }
