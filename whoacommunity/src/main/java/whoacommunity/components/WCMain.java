841dfc77cc83ee1e01f01ba8871903db6475ad19
Hugi Thordarson <hugi@karlmenn.is>
2025-07-06 10:30:58 +0000

    Basic article display on the start page

--- whoacommunity/src/main/java/whoacommunity/components/WCMain.java
+++ whoacommunity/src/main/java/whoacommunity/components/WCMain.java
@@ -1,11 +1,26 @@
 package whoacommunity.components;
 
+import java.util.List;
+
+import org.apache.cayenne.query.ObjectSelect;
+
 import ng.appserver.NGContext;
 import ng.appserver.templating.NGComponent;
+import whoacommunity.app.WCCore;
+import whoacommunity.data.Article;
 
 public class WCMain extends NGComponent {
 
+	public Article currentArticle;
+
 	public WCMain( NGContext context ) {
 		super( context );
+	}
+
+	public List<Article> articles() {
+		return ObjectSelect
+				.query( Article.class )
+				.orderBy( Article.DATE.desc() )
+				.select( WCCore.newContext() );
 	}
 }
