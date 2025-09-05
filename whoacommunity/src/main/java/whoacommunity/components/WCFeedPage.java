5a1a8f3b10d1c905cedddc229214b6583665d6fa
Hugi Thordarson <hugi@karlmenn.is>
2025-09-05 11:11:12 +0000

    Nicer management and display of repositories we read atom feeds from

--- whoacommunity/src/main/java/whoacommunity/components/WCFeedPage.java
+++ whoacommunity/src/main/java/whoacommunity/components/WCFeedPage.java
@@ -1,5 +1,6 @@
 package whoacommunity.components;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import com.apptasticsoftware.rssreader.Item;
@@ -7,28 +8,82 @@ import com.apptasticsoftware.rssreader.RssReader;
 
 import ng.appserver.NGContext;
 import whoacommunity.app.WCComponent;
+import whoacommunity.util.Repos;
+import whoacommunity.util.Repos.Org;
+import whoacommunity.util.Repos.Repo;
 
 public class WCFeedPage extends WCComponent {
 
-	public Item currentItem;
+	public OurItem current;
+	public Org currentOrg;
+	public Repo currentRepo;
 
 	public WCFeedPage( NGContext context ) {
 		super( context );
 	}
 
-	public List<Item> items() {
-		final List<String> urls = List.of(
-				"https://github.com/undur/wonder-slim/commits.atom",
-				"https://github.com/undur/parsley/commits.atom",
-				"https://github.com/undur/modulo/commits.atom",
-				"https://github.com/undur/wonder-slim-deployment/commits.atom",
-				"https://github.com/undur/vermilingua-maven-plugin/commits.atom",
-				//				"https://github.com/undur/whoacommunity/commits.atom",
-				"https://github.com/undur/examiner/commits.atom" );
+	public Org[] orgs() {
+		return Org.values();
+	}
+
+	public List<OurItem> items() {
+		final List<String> urls = Repos.repos()
+				.stream()
+				.map( Repo::commitsAtomURL )
+				.toList();
 
 		return new RssReader()
 				.read( urls )
 				.sorted()
+				.map( OurItem::new )
 				.toList();
+	}
+
+	/**
+	 * Wrapper for the atom feed's item so we can enrich it with a bit of our own data
+	 */
+	public record OurItem( Item item ) {
+
+		/**
+		 * @return The repo from which this message originated
+		 */
+		public Repo repo() {
+			for( Repo repo : Repos.repos() ) {
+				if( item.getLink().get().startsWith( repo.url() ) ) {
+					return repo;
+				}
+			}
+
+			return null;
+		}
+
+		/**
+		 * @return Date for display
+		 */
+		public String dateFormatted() {
+			return item().getPubDateZonedDateTime().get().format( DateTimeFormatter.ofPattern( "yyyy-MM-dd" ) );
+		}
+
+		/**
+		 * Hide away the Optional
+		 */
+		public String link() {
+			return item().getLink().orElse( null );
+		}
+
+		/**
+		 * Hide away the Optional
+		 */
+		public String title() {
+			return item().getTitle().orElse( null );
+		}
+
+		/**
+		 * Hide away the Optional
+		 */
+		public String author() {
+			final String author = item().getAuthor().orElse( null );
+			return "hugithordarson".equals( author ) ? "hugi" : author;
+		}
 	}
 }
---
+++ whoacommunity/src/main/java/whoacommunity/util/Repos.java
@@ -0,0 +1,65 @@
+package whoacommunity.util;
+
+import java.util.List;
+
+/**
+ * For keeping track of github repositories we follow
+ */
+
+public class Repos {
+
+	/**
+	 * The list of repositories we follow
+	 */
+	private static List<Repo> _repos;
+
+	/**
+	 * @return The list of repositories we follow
+	 */
+	public static List<Repo> repos() {
+		if( _repos == null ) {
+			_repos = List.of(
+					// Undur repos
+					new Repo( Org.undur, "🤸‍♀️", "wonder-slim", "https://github.com/undur/wonder-slim" ),
+					new Repo( Org.undur, "🌿", "parsley", "https://github.com/undur/Parsley" ),
+					new Repo( Org.undur, "🤖", "modulo", "https://github.com/undur/modulo" ),
+					new Repo( Org.undur, "🎛️", "wonder-slim-deployment", "https://github.com/undur/wonder-slim-deployment" ),
+					new Repo( Org.undur, "🦡", "vermilingua", "https://github.com/undur/vermilingua-maven-plugin" ),
+					new Repo( Org.undur, "📚", "whoacommunity.com", "https://github.com/undur/whoacommunity" ),
+					new Repo( Org.undur, "👨‍⚕️", "examiner", "https://github.com/undur/examiner" ),
+
+					// Cayenne repos
+					new Repo( Org.cayenne, "🌶", "cayenne", "https://github.com/apache/cayenne" ),
+
+					// WOCommunity repos
+					new Repo( Org.wocommunity, "🛠️", "wolips", "https://github.com/wocommunity/wolips" ),
+					new Repo( Org.wocommunity, "📜", "wonder", "https://github.com/wocommunity/wonder" ) );
+		}
+
+		return _repos;
+	}
+
+	public enum Org {
+		undur,
+		wocommunity,
+		cayenne;
+
+		public List<Repo> repos() {
+			return Repos
+					.repos()
+					.stream()
+					.filter( f -> f.organization() == this )
+					.toList();
+		}
+	}
+
+	public record Repo( Org organization, String emoji, String name, String url ) {
+
+		/**
+		 * @return The URL for the repo's atom commit feed
+		 */
+		public String commitsAtomURL() {
+			return url() + "/commits.atom";
+		}
+	}
+}
