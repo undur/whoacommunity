3faa0d3fc95661702b86c0c4117f2936e33f22b3
Hugi Thordarson <hugi@karlmenn.is>
2025-11-02 12:35:54 +0000

    Cache the inside.java RSS-feed, for now

--- whoacommunity/src/main/java/whoacommunity/components/WCSidebar.java
+++ whoacommunity/src/main/java/whoacommunity/components/WCSidebar.java
@@ -13,24 +13,35 @@ import whoacommunity.app.WCComponent;
 public class WCSidebar extends WCComponent {
 
 	public JavaItem javaItem;
+	private static List<JavaItem> _javaItems;
 
 	public WCSidebar( NGContext context ) {
 		super( context );
 	}
 
+	/**
+	 * FIXME: We're temporarily caching this forever for performance, add a more intelligent cache // Hugi 2025-11-02
+	 */
 	public List<JavaItem> javaItems() {
-		try {
-			return new RssReader()
-					.read( "https://inside.java/feed.xml" )
-					.sorted()
-					.map( JavaItem::new )
-					.toList();
+		if( _javaItems == null ) {
+			try {
+				_javaItems = new RssReader()
+						.read( "https://inside.java/feed.xml" )
+						.sorted()
+						.map( JavaItem::new )
+						.toList();
+			}
+			catch( IOException e ) {
+				_javaItems = List.of();
+			}
 		}
-		catch( IOException e ) {
-			return List.of();
-		}
+
+		return _javaItems;
 	}
 
+	/**
+	 * FIXME: use a single wrapper class (or at least a common interface) for our RSS items // Hugi 2025-11-02
+	 */
 	public record JavaItem( Item item ) {
 
 		public String title() {
@@ -41,9 +52,6 @@ public class WCSidebar extends WCComponent {
 			return item.getLink().get();
 		}
 
-		/**
-		 * @return Date for display
-		 */
 		public String shortDateFormatted() {
 			return item().getPubDateZonedDateTime().get().format( DateTimeFormatter.ofPattern( "MMM d" ) );
 		}
