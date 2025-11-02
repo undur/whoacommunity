d5b3ee1916d75d811a0ee52bef3f8f18860176c6
Hugi Thordarson <hugi@karlmenn.is>
2025-11-02 12:26:22 +0000

    Ugly, hacky RSS feed reader

--- whoacommunity/src/main/java/whoacommunity/components/WCSidebar.java
+++ whoacommunity/src/main/java/whoacommunity/components/WCSidebar.java
@@ -1,11 +1,47 @@
 package whoacommunity.components;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.apptasticsoftware.rssreader.Item;
+import com.apptasticsoftware.rssreader.RssReader;
+
 import ng.appserver.NGContext;
 import whoacommunity.app.WCComponent;
 
 public class WCSidebar extends WCComponent {
 
+	public JavaItem javaItem;
+
 	public WCSidebar( NGContext context ) {
 		super( context );
+	}
+
+	public List<JavaItem> javaItems() {
+		try {
+			return new RssReader()
+					.read( "https://inside.java/feed.xml" )
+					.sorted()
+					.map( JavaItem::new )
+					.toList();
+		}
+		catch( IOException e ) {
+			return List.of();
+		}
+	}
+
+	public record JavaItem( Item item ) {
+
+		public String title() {
+			return item.getTitle().get();
+		}
+
+		public String link() {
+			return item.getLink().get();
+		}
+
+		public String shortDateFormatted() {
+			return "heh";
+		}
 	}
 }
